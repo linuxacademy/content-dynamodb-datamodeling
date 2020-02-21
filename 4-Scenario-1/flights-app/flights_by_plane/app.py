adf2e699a23e523f34635c2e889d7429d5b57848
Mark Richman <mark@markrichman.com>
2020-02-21 10:43:46 -0500

    Added pagination

--- 4-Scenario-1/flights-app/flights_by_plane/app.py
+++ 4-Scenario-1/flights-app/flights_by_plane/app.py
@@ -11,11 +11,23 @@ def lambda_handler(event, context):
         print(event)
         plane = event["queryStringParameters"]["plane"]
         result = table.query(
-            IndexName="plane-index", KeyConditionExpression=Key("plane_iata").eq(plane)
+            IndexName="plane_iata-index",
+            KeyConditionExpression=Key("plane_iata").eq(plane),
         )
+
+        data = result["Items"]
+
+        while "LastEvaluatedKey" in result:
+            result = table.query(
+                IndexName="plane_iata-index",
+                KeyConditionExpression=Key("plane_iata").eq(plane),
+                ExclusiveStartKey=result["LastEvaluatedKey"],
+            )
+            data.update(result["Items"])
+
         response = {
             "statusCode": 200,
-            "body": json.dumps(result["Items"][0]),
+            "body": json.dumps({"count": len(data), "data": data}),
             "headers": {"Content-Type": "application/json"},
         }
         return response
