9ed429b5f2bf1429bd5432b46b2737a2b99d696e
Mark Richman <mark@markrichman.com>
2020-02-21 06:26:29 -0500

    Added SAM template

---
+++ 4-Scenario-1/flights-app/airport_by_name/app.py
@@ -0,0 +1,30 @@
+import json
+
+import boto3
+from boto3.dynamodb.conditions import Key
+
+table = boto3.resource("dynamodb").Table("flights")
+
+
+def lambda_handler(event, context):
+    try:
+        print(event)
+        result = table.query(
+            IndexName="src_ap-index",
+            KeyConditionExpression=Key("src_ap").eq(
+                event["queryStringParameters"]["name"]
+            ),
+            Limit=1,
+        )
+        response = {
+            "statusCode": 200,
+            "body": json.dumps(result["Items"][0]["PK"]),
+            "headers": {
+                "Content-Type": "application/json",
+                "Access-Control-Allow-Origin": "*",
+            },
+        }
+        return response
+    except Exception as e:
+        print(e)
+        raise e
