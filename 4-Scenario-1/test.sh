d049180131e4dafb15d0d51ca8993877b1befb71
Mark Richman <mark@markrichman.com>
2020-02-21 21:14:47 -0500

    Formatting

--- 4-Scenario-1/test.sh
+++ 4-Scenario-1/test.sh
@@ -6,24 +6,17 @@ REGION=us-east-1
 APIID=$(aws apigateway get-rest-apis --query "items[?name==\`${APINAME}\`].id" --output text --region ${REGION})
 URL="https://${APIID}.execute-api.${REGION}.amazonaws.com/Prod/"
 
-echo
-echo "Finding flights by plane"
-echo
+echo ">>> Finding flights by plane\n"
 curl "${URL}/flightsByPlane/?plane=747"
-echo
-echo "Finding airport by code"
-echo
+
+echo "\n\n>>> Finding airport by code\n"
 curl "${URL}/airportByCode/?code=MCO"
-echo
-echo "Finding airport by name"
-echo
+
+echo "\n\n>>> Finding airport by name\n"
 curl "${URL}/airportByName/?name=Orlando%20International%20Airport"
-echo
-echo "Finding flights by codes"
-echo
+
+echo "\n\n>>> Finding flights by codes\n"
 curl "${URL}/flightsByCodes/?src=MCO&dst=FLL"
-echo
-echo "Finding outbound flights by airport"
-echo
+
+echo "\n\n>>> Finding outbound flights by airport\n"
 curl "${URL}/outboundByAirport/?code=MCO"
-echo
